c3c2645cb9fa348480fbec05f2224a7263502138
xpigm <7346354+xpigm@users.noreply.github.com>
2022-05-12 15:21:36 +0800

    Update wave.php

--- php/wave.php
+++ php/wave.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type:text/json;charset=UTF-8");
 date_default_timezone_set("PRC");
-$data=file_get_contents("../bolang.js");
+$data=file_get_contents("../js/bolang.js");
 echo $data;
 ?>
